995685f3fbd9496f484429cd03e94110c982dac6
ximimiao <ximimiao@163.com>
2019-02-01 22:47:46 +0800

    二维数组中找数字

---
+++ bubblesort.py
@@ -0,0 +1,29 @@
+def bubblesort(numList):
+    """"""
+    # 优化前，时间复杂度o(n2)
+    for i in range (len(numList) -1):
+        for j in range(i,len(numList)-1):
+            if numList[j] > numList [j+1]:
+                numList[j],numList[j+1] = numList[j+1],numList[j]
+        print(numList)
+def optibubbulesort(numList):
+    """
+    优化后
+    :param numList:
+    :return:
+    """
+    for i in range (len(numList) -1):
+        bbalance = False
+        for j in range(i,len(numList)-1):
+            if numList[j] > numList [j+1]:
+                numList[j],numList[j+1] = numList[j+1],numList[j]
+                bbalance = True
+        print(numList)
+        if bbalance is not True:
+            break
+    return numList
+
+
+if __name__ == '__main__':
+    listnum = [1,2,5,7,3,9,4]
+    optibubbulesort(listnum)
